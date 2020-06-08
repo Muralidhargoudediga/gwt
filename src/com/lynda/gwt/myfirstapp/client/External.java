7b5fdedd7051e8dddc88dd557fcbce46ebf02cf9
Muralidhargoudediga <muralidhargoud.ediga@gmail.com>
2020-06-08 14:27:00 +0530

    Working with resources

---
+++ src/com/lynda/gwt/myfirstapp/client/External.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.lynda.gwt.myfirstapp.client;
+
+import com.google.gwt.resources.client.CssResource;
+
+/**
+ * @author kylegowerwinter
+ *
+ */
+public interface External extends CssResource {
+
+	String important();
+
+	String bigRedBorder();
+	
+	String underline();
+
+}
